9fcf46c99bed687d448455997f4b01a605cfba20
Edward Felch <efelch@gmail.com>
2015-07-26 22:56:47 -0400

    more css cleanup

--- js/theme.js
+++ js/theme.js
@@ -1,8 +1,6 @@
 $(document).ready(function(){
     var classCycle=['background-image-1','background-image-2','background-image-3','background-image-4','background-image-5','background-image-6','background-image-7','background-image-8','background-image-9',];
-
     var randomNumber = Math.floor(Math.random() * classCycle.length);
-    var classToAdd = classCycle[randomNumber];
-    
+    var classToAdd = classCycle[randomNumber];    
     $('body').addClass(classToAdd);
 });
